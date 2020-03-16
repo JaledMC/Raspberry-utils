43d08b6623d697f1514ff0fbf111db4281bbd157
betegon <miguelbetegongarcia@gmail.com>
2020-03-16 10:48:35 +0100

    change crop videos

--- crop_videos.py
+++ crop_videos.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python
-'''
-Script to crop a video in files of a determined lenght.
-'''
-
 
 import subprocess
 import re
@@ -10,57 +6,35 @@ import math
 from optparse import OptionParser
 
 
-length_regexp = 'Duration: (\d{2}):(\d{2}):(\d{2})\.\d+,'
+length_regexp = b'Duration: (\d{2}):(\d{2}):(\d{2})\.\d+,'
 re_length = re.compile(length_regexp)
-
-def parse_options():
-    parser = OptionParser()
-
-    parser.add_option("-f", "--file",
-                        dest = "filename",
-                        required = true,
-                        type = str,
-                        help = "file to split, for example sample.avi",
-                        type = "string",
-                        action = "store"
-                        )
-    parser.add_option("-s", "--split-size",
-                        dest = "split_size",
-                        required = true
-                        help = "split or chunk size in seconds, for example 10",
-                        type = "int",
-                        action = "store"
-                        )
-    (options, args) = parser.parse_args()
-
-
 
 def main():
 
     (filename, split_length) = parse_options()
     if split_length <= 0:
-        print "Split length can't be 0"
+        print ("Split length can't be 0")
         raise SystemExit
 
     output = subprocess.Popen("ffmpeg -i '"+filename+"' 2>&1 | grep 'Duration'",
                             shell = True,
                             stdout = subprocess.PIPE
                             ).stdout.read()
-    print output
+    print (output)
     matches = re_length.search(output)
     if matches:
         video_length = int(matches.group(1)) * 3600 + \
                         int(matches.group(2)) * 60 + \
                         int(matches.group(3))
-        print "Video length in seconds: "+str(video_length)
+        print ("Video length in seconds: {}".format(video_length))
     else:
-        print "Can't determine video length."
+        print ("Can't determine video length.")
         raise SystemExit
 
-    split_count = int(math.ceil(video_length/float(split_length)))
+    split_count = math.ceil(video_length/float(split_length))
     print(split_count)
     if(split_count == 1):
-        print "Video length is less then the target split length."
+        print ("Video length is less then the target split length.")
         raise SystemExit
 
     split_cmd = "ffmpeg -i '"+filename+"' -vcodec copy "
@@ -74,22 +48,29 @@ def main():
         split_str += " -ss "+str(split_start)+" -t "+str(split_length) + \
                     " '"+filename[:-4] + "---" + str(n) + "." + filename[-3:] + \
                     "'"
-        print "About to run: "+split_cmd+split_str
+        print ("About to run: {}{}".format(split_cmd, split_str))
         output = subprocess.Popen(split_cmd+split_str, shell = True, stdout =
                                subprocess.PIPE).stdout.read()
 
-    if options.filename and options.split_size:
 
-        return (options.filename, options.split_size)
+def parse_options():
+    parser = OptionParser()
 
-    else:
-        parser.print_help()
-        raise SystemExit
+    parser.add_option("-f", "--file",
+                        dest = "filename",
+                        help = "file to split, for example sample.avi",
+                        type = str,
+                        action = "store"
+                        )
+    parser.add_option("-s", "--split-size",
+                        dest = "split_size",
+                        help = "split or chunk size in seconds, for example 10",
+                        type = int,
+                        action = "store"
+                        )
+    (options, args) = parser.parse_args()
+    return (options.filename, options.split_size)
+
 
 if __name__ == '__main__':
-
-    try:
         main()
-    except Exception, e:
-        print "Exception occured running main():"
-        print str(e)
