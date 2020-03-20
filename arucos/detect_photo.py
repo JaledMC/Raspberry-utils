7330fbb92d5094fca3a02f136d1d616e078fb17c
JaledMC <j.moustafa@sialitech.com>
2020-03-20 17:35:36 +0100

    pep8

--- arucos/detect_photo.py
+++ arucos/detect_photo.py
@@ -1,11 +1,11 @@
 import argparse
 import numpy as np
-import cv2, PIL
+import cv2
+import PIL
 from cv2 import aruco
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import pandas as pd
-
 
 
 def parser():
@@ -30,16 +30,16 @@ args = parser()
 frame = cv2.imread(args.aruco_path)
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
-parameters =  aruco.DetectorParameters_create()
+parameters = aruco.DetectorParameters_create()
 corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
 frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
 
 plt.figure()
 plt.imshow(cv2.cvtColor(frame_markers, cv2.COLOR_BGR2RGB))
-if hasattr(ids,'any'):
+if hasattr(ids, 'any'):
     for i in range(len(ids)):
         c = corners[i][0]
-        plt.plot([c[:, 0].mean()], [c[:, 1].mean()], "o", label = "id={0}".format(ids[i]))
+        plt.plot([c[:, 0].mean()], [c[:, 1].mean()], "o", label="id={0}".format(ids[i]))
     plt.legend()
     plt.show()
 else:
