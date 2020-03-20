7a52a55cbc7fe3f3e60e47e4982de4a33c1cf0d0
JaledMC <j.moustafa@sialitech.com>
2020-03-20 19:02:33 +0100

    spreasheet refactor and example
    Using argparse instead config file (faster and easier), and small example in the main

--- PythonSpreadsheet/csvToSpredsheet.py
+++ PythonSpreadsheet/csvToSpredsheet.py
@@ -1,24 +1,48 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
-import config
 
 """
+Script para subir con Python valores a una hoja de Google Spreadsheets
 Necesario: Google developer console Google Drive API y Google Sheets API
 Obtener JSON para tener credenciales y poder modificar el spreadsheet
 """
 
-scope = config.SCOPE
-credentials = ServiceAccountCredentials.from_json_keyfile_name(config.CREDENTIALS, config.SCOPE)
-gc = gspread.authorize(credentials)
-sheet = gc.open_by_key(config.KEY)
-sheet1 = sheet.get_worksheet(0)
+SCOPE = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive',
+         'https://www.googleapis.com/auth/spreadsheets']
 
-def add_row(row):
-    """Append rows to an spreadsheet using gspread:
-    Args:
-        row (list): List of parameters to append.
-    Returns:
-        None
-    """
-    sheet1.append_row(row)
+
+def parse_options():
+    parser = argparse.ArgumentParser()
+    parser.add_option(
+        "-c", "--credentials",
+        help="path to authentifiaction json crendentials",
+    )
+    parser.add_option(
+        "-k", "--key",
+        help="key to the spreadsheet. Usually a part of the URL",
+        )
+    parser.add_option(
+        "-s", "--sheet",
+        help="number of the sheet to be written",
+        default=0,
+    )
+    return parser.parse_args()
+
+
+def init_sheet(credentials_path, key, work_sheet):
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(credentials_path, SCOPE)
+    gc = gspread.authorize(credentials)
+    return gc.open_by_key(key).get_worksheet(work_sheet)
+
+
+def main():
+    args = parse_options()
+    sheet = init_sheet(args.credentials, args.key, args.sheet)
+    for i in range(10):
+        sheet.append_row(["Prueba number: ".format(i)])
+
+
+if __name__ == "__main__":
+    main()
